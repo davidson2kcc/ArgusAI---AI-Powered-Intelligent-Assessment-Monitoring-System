24be478a4f7766a6b83f64744b904eb79f03c234
davidson2kcc <alagiripraveen@gmail.com>
2026-07-22 15:16:44 +0530

    Resolve backend merge conflict

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -13,28 +11,14 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.get("/health")
-def health():
-    return {"status": "OK"}
-
-=======
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+@app.get("/")
+def home():
+    return {
+        "message": "AI Assessment Monitoring Backend is Running"
+    }
 
 @app.get("/health")
 def health():
-    return {"status": "OK"}
-
->>>>>>> 831323a149d61e92dabdaf143f0657df65d465ec
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+    return {
+        "status": "OK"
+    }
